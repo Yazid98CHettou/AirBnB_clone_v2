39125152f56ed66b63d7378777db4eecf94f3e84
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-05-03 18:47:01 -0700

    0x03. AirBnB clone - Deploy static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""generate a .tgz archive with this script"""
+
+from fabric.api import local
+import time
+import os
+
+
+def do_pack():
+    time_string = time.strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(timestamp)
+    try:
+        if not os.path.exists("versions"):
+            local("mkdir -p versions")
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except Exception as e:
+        print(e)
+        return None
+
